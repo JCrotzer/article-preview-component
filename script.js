9b24ca2550747122cbd0d5aece7ce0305053f0b4
Julie Crotzer <jcrotzerrn@aol.com>
2022-08-24 08:57:40 -0500

    updated css and html

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const btn = document.querySelector('.share-btn');
+const shareSocial = document.getElementById('share-social-icons');
+
+function shareSocialMedia() {
+    shareSocial.classList.toggle('active');
+}
+
+btn.addEventListener('click', shareSocialMedia);
+
+
